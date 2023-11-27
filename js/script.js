9df0f8d9ec3044e13cfbd16216275c7a4f9d8a15
Johuttt <2010631170149@student.unsika.ac.id>
2023-11-27 16:16:29 +0700

    mencoba menangkap API tapi tak bisa

--- js/script.js
+++ js/script.js
@@ -26,3 +26,53 @@ document.addEventListener('click', function (event) {
         dropdown.classList.remove('show');
     }
 });
+
+
+//mencoba utuk menangkap API
+
+        // Mengambil data dari API
+        function getArticles() {
+            const url = "https://newsdata.io/api/1/news?country=id&apikey=pub_335767a0176944d927e96151a9c852003ebe0";
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.onload = function () {
+                if (xhr.status === 200) {
+                    const articles = JSON.parse(xhr.responseText);
+                    for (let i = 0; i < 5; i++) {
+                        addArticles(articles[i]);
+                    }
+                } else {
+                    console.error("Error fetching articles: " + xhr.status);
+                }
+            };
+            xhr.send();
+        }
+
+        // Menambahkan artikel ke halaman
+        function addArticles(articles) {
+            if (articles && Array.isArray(articles) && articles.length > 0) {
+                const container = document.querySelector(".articles");
+
+                for (let i = 0; i < articles.length; i++) {
+                    const article = articles[i];
+
+                    if (article && article.image_url && article.image_url !== "") {
+                        const articleElement = document.createElement("article");
+                        articleElement.innerHTML = `
+                    <img src="${article.image_url}" alt="${article.title}">
+                    <h2>${article.title}</h2>
+                    <p>${article.category}</p>
+                `;
+                        container.appendChild(articleElement);
+                        console.log(article);
+                    }
+                }
+            } else {
+                console.error("No valid articles to display.");
+            }
+        }
+
+
+
+        // Memulai fungsi getArticles()
+        getArticles();
